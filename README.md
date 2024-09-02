18b232bae33813c889c233033ad7559f3ced2907
zeuss1102 <emiliopuig3@gmail.com>
2024-09-02 14:03:14 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# reto1_donut_app_emilio_puigcerver
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/reto1_donut_app_emilio_puigcerver/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.reto1_donut_app_emilio_puigcerver
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
