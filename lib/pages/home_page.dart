25947456793b0cefbe8cc036915f8ef6f5df56e2
zeuss1102 <149445249+zeuss1102@users.noreply.github.com>
2024-09-05 15:15:25 -0600

    añadiendo texto principal

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,8 +13,34 @@ class _HomePageState extends State<HomePage> {
     return  Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        leading: Icon(Icons.menu, color: Colors.grey[800]),
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 24.0),
+          child: IconButton(icon: Icon(Icons.menu, color: Colors.grey[800], size: 36,), onPressed: () {
+             print('botón de menú');},
+              ),
+        ),
+        actions: [Padding(
+          padding: const EdgeInsets.only(right: 24.0),
+          child: IconButton(icon: Icon(Icons.person, color: Colors.grey [800], size: 36,), onPressed: () {
+             print('boton de persona');},
+           ),
+        )],
       ),
+      body:const Column(children: [
+        //texto "i want to Eat"
+        Padding(
+          padding:  EdgeInsets.all(24.0),
+          child: Row(
+            children: [
+              Text('I want to ', style: TextStyle(fontSize: 32),),
+              Text('Eat', style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, decoration: TextDecoration.underline),),
+            ],
+          ),
+        )
+        //tab bar
+
+        //tab bar view
+      ],),
     );
   }
 }
